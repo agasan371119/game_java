d7533f8c23d647bf5855b16734d97041971fa736
agasan371119 <yna77351unichi@icloud.com>
2020-08-04 15:32:04 +0900

    練習１

---
+++ Part01.java
@@ -0,0 +1,11 @@
+public class Part01 {
+  public static void main (String[] args) {
+    String name = "王子";
+    int lv = 100 + 255;
+  System.out.println("魔王が" + "世界を滅ぼそうとしています！");
+    System.out.println(name + "はレベル" + lv + "の勇者を召喚した");
+    System.out.println(name + "は、魔王を倒した");
+    System.out.println("レベル" + lv + "楽勝でした！！！");
+    System.out.println("GAME CLAER!");
+  }
+}
