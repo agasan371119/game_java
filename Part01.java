989fd48402134916e8304d737c64d48b919bf15e
agasan371119 <yna77351unichi@icloud.com>
2020-08-06 08:12:32 +0900

    練習3

--- Part01.java
+++ Part01.java
@@ -1,11 +1,15 @@
 public class Part01 {
   public static void main (String[] args) {
     String name = "王子";
-    int lv = 80;
-  System.out.println("魔王が" + "世界を滅ぼそうとしています！");
-    System.out.println(name + "はレベル" + lv + "の勇者を召喚した");
-    System.out.println(name + "は、魔王を倒した");
-    System.out.println("レベル" + lv + "楽勝でした！！！");
+    int lv = 29;
+      System.out.println("魔王が" + "世界を滅ぼそうとしています！");
+      System.out.println(name + "はレベル" + lv + "の勇者を召喚した");
+      System.out.println(name + "は、魔王を倒した");
+    if(lv > 50) {
+      System.out.println("レベル" + lv + "なので倒せました！！！");
+    } else {
+      System.out.println("レベル" + lv + "なので苦戦しました！！！");
+    }
     System.out.println("GAME CLAER!");
   }
 }
