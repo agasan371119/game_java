ba96ee7d70425db30dcbce3f6bd11525a28ad109
agasan371119 <yna77351unichi@icloud.com>
2020-08-10 13:01:13 +0900

    練習8

--- Part01.java
+++ Part01.java
@@ -4,15 +4,16 @@ public class Part01 {
   }
 
   static void putJyosyou() {
-    String name1 = "王様";
-    String name2 = "召喚士";
-    
+    String[] team = {"王様", "召喚士", "勇者"};
+
     int lv = (int)(Math.random() * 100 + 1);
 
     System.out.println("魔王が" + "世界を滅ぼそうとしています！");
 
-    System.out.println(name1 + "は、世界を救うため" + name2 + "に命令を下した！！");
-    System.out.println(name2 + "はレベル" + lv + "の勇者を召喚した");
+    System.out.println(team[0] + "は、世界を救うため" + team[1] + "に命令を下した！！");
+    System.out.println(team[1] + "はレベル" + lv + "の" + team[2] + "を召喚した");
+
+    System.out.println("魔王が現れた！！" + team[2] + "は、聖剣エクスカリバーを構えた！！！！");
 
     if(lv > 50) {
       System.out.println("レベル" + lv + "なので余裕で倒せました！！！");
@@ -20,11 +21,11 @@ public class Part01 {
 
     } else if(lv == 25){
       System.out.println("レベル" + lv + "なので苦戦しましたが、なんとか倒せました！！！");
-      System.out.println("GAME CLAER!");
+      System.out.println("TRUE END!");
 
     } else {
       System.out.println("レベル" + lv + "なので負けました(ぴえん)");
-      System.out.println("GAME OVER!");
+      System.out.println("BAD END");
     }
   }
 }
