b68a39be808d62e86fbec2cad39849c6b21c2270
agasan371119 <yna77351unichi@icloud.com>
2020-08-15 09:33:49 +0900

    練習18

--- Part01.java
+++ Part01.java
@@ -4,7 +4,7 @@ public class Part01 {
   static String[] team = {"王様", "召喚士", "勇者"};
   static String[] enemy = {"ドラゴン", "魔王", "竜王"};
   static String[] magic = {"ファイヤ", "サンダー", "ブリザード"};
-  static String[] end = {"HAPPY END", "TRUE END", "BAD END"};
+  static String[] end = {"HAPPY END？", "TRUE END", "BAD END"};
 
 
   static int lv = (int)(Math.random() * 100 + 1);
@@ -32,13 +32,13 @@ public class Part01 {
     System.out.println(enemy[1] +  "が現れた！！" + team[2] + "は、" + magic[0] + "を唱えた！！！！");
 
     if(lv > 50) {
-      System.out.println("レベル" + lv + "なので余裕でした！！！");
-      System.out.println(team[2] + "は、姫を救い、影で" + team[0] + "を暗殺して、姫と結婚し王国を乗っ取った！");
+      System.out.println("レベル" + lv + "なので一瞬で抹殺した！！！");
+      System.out.println(team[2] + "は、姫を救うように見せかけ、裏で" + team[0] + "を暗殺して王国を裏で操った！");
       System.out.println(end[0]);
 
     } else if(lv == 25){
       System.out.println("レベル" + lv + "なので苦戦しましたが、なんとか倒せました！！！");
-      System.out.println(team[0] + "に、用済みと宣告を受け暗殺されました");
+      System.out.println(team[0] + "に、用済みと宣告を受け強制的に元の世界に帰されました");
       System.out.println(end[1]);
 
     } else {
