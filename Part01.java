882d118df094a171450fd43ff468a6ee4b6ac9fa
agasan371119 <yna77351unichi@icloud.com>
2020-09-05 00:40:27 +0900

    練習38

--- Part01.java
+++ Part01.java
@@ -17,9 +17,9 @@ public class Part01 {
   }
 
   static void prologue() {
-    System.out.println(enemy[en] + "は、世界を滅ぼそうとしています！");
-    System.out.println(enemy[en] + "の配下は、姫をさらって行った");
-    System.out.println("姫を攫われて" + team[en] + "は、頭を抱えている");
+    System.out.println(enemy[1] + "は、世界を滅ぼそうとしています！");
+    System.out.println(enemy[1] + "の配下は、姫をさらって行った");
+    System.out.println("姫を攫われて" + team[0] + "は、頭を抱えている");
   }
 
   static void summon() {
